6b0082d8e68c4124e40a14a61dc98bbde9a4d864
MatejCharbusky <58331664+MatejCharbusky@users.noreply.github.com>
2023-04-15 13:45:54 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar color="primary" dark app>
+      <router-link to="/">
+        <v-toolbar-title class="page-title">Formátovač v4.0</v-toolbar-title>
+      </router-link>
+    </v-app-bar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: () => ({}),
+};
+</script>
+
+<style scoped>
+.page-title {
+  color: #fff;
+  font-weight: bold;
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container fluid>Hello view</v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
+
+
